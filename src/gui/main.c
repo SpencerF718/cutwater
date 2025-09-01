71ade4afa4ebf977b52d1ad4e904473fedb3a5c5
SpencerF718 <194403957+SpencerF718@users.noreply.github.com>
2025-08-31 23:18:17 -0400

    Add text input to the window.

--- src/gui/main.c
+++ src/gui/main.c
@@ -5,11 +5,26 @@ activate (GtkApplication *app,
           gpointer        user_data)
 {
   GtkWidget *window;
+  GtkWidget *scrolled_window;
+  GtkWidget *text_view;
 
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-  gtk_window_present (GTK_WINDOW (window));
+  gtk_window_set_title (GTK_WINDOW (window), "Cutwater");
+  gtk_window_set_default_size (GTK_WINDOW (window), 400, 400);
+
+  scrolled_window = gtk_scrolled_window_new();
+  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),
+                                 GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+
+  text_view = gtk_text_view_new();
+  gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD_CHAR);
+
+  gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled_window), text_view);
+
+  gtk_window_set_child(GTK_WINDOW(window), scrolled_window);
+
+  gtk_window_present(GTK_WINDOW(window));
+
 }
 
 int
@@ -19,8 +34,9 @@ main (int    argc,
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  app = gtk_application_new ("org.gtk.cutwater", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
 
