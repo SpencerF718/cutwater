539144a5932f18421759741d39a9a86ca9ffc9f0
SpencerF718 <194403957+SpencerF718@users.noreply.github.com>
2025-09-04 10:46:14 -0400

    Add markdown preview pane with heading1 support

--- src/gui/main.c
+++ src/gui/main.c
@@ -1,6 +1,7 @@
 #include <gtk/gtk.h>
 #include "cutwater.h"
 #include "file_io.h"
+#include <string.h>
 
 void update_window_title(CutwaterApp *app) {
   const char *filename = app->current_file ? app->current_file : "Untitled";
@@ -11,12 +12,60 @@ void update_window_title(CutwaterApp *app) {
   g_free(title);
 }
 
+static void render_markdown_to_preview(GtkTextBuffer *src, GtkTextBuffer *dest) {
+  GtkTextIter start, end;
+  gtk_text_buffer_get_start_iter(src, &start);
+  gtk_text_buffer_get_end_iter(src, &end);
+
+  char *text = gtk_text_buffer_get_text(src, &start, &end, FALSE);
+
+  gtk_text_buffer_set_text(dest, "", -1);
+
+  GtkTextIter dest_iter;
+  gtk_text_buffer_get_start_iter(dest, &dest_iter);
+
+  const char *p = text;
+
+  while (*p) {
+    if (g_str_has_prefix(p, "# ")) {
+      p += 2;
+      const char *line_end = strchr(p, '\n');
+      if (!line_end) line_end = p + strlen(p);
+
+      gtk_text_buffer_insert_with_tags_by_name(dest,
+                                               &dest_iter, p, line_end - p, "heading1", NULL);
+      gtk_text_buffer_insert(dest, &dest_iter, "\n", -1);
+
+      p = line_end;
+    } else {
+      const char *line_end = strchr(p, '\n');
+      if (!line_end) line_end = p + strlen(p);
+
+      gtk_text_buffer_insert(dest, &dest_iter, p, line_end - p);
+      gtk_text_buffer_insert(dest, &dest_iter, "\n", -1);
+
+      p = line_end;
+    }
+
+    if (*p == '\n') p++;
+  }
+
+  g_free(text);
+
+}
+
+
 static void on_buffer_changed(GtkTextBuffer *buffer, gpointer user_data) {
   CutwaterApp *app = user_data;
   if (!app->modified) {
     app->modified = TRUE;
     update_window_title(app);
   }
+}
+
+static void on_editor_changed(GtkTextBuffer *buffer, gpointer user_data) {
+  GtkTextBuffer *preview_buffer = GTK_TEXT_BUFFER(user_data);
+  render_markdown_to_preview(buffer, preview_buffer);
 }
 
 static void cutwater_app_free(CutwaterApp *app) {
@@ -29,21 +78,32 @@ static void activate(GtkApplication *gapp, gpointer user_data) {
   CutwaterApp *app = g_new0(CutwaterApp, 1);
 
   GtkWidget *window;
-  GtkWidget *scrolled_window;
+  GtkWidget *paned;
+  GtkWidget *scrolled_editor;
+  GtkWidget *scrolled_preview;
   GtkWidget *text_view;
+  GtkWidget *preview_view;
 
   window = gtk_application_window_new(gapp);
   gtk_window_set_default_size(GTK_WINDOW(window), 600, 500);
 
-  scrolled_window = gtk_scrolled_window_new();
-  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),
-                                 GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-
+  // Split view
+  paned = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
+  gtk_window_set_child(GTK_WINDOW(window), paned);
+ 
+  // Editor
+  scrolled_editor = gtk_scrolled_window_new();
   text_view = gtk_text_view_new();
   gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD_CHAR);
+  gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled_editor), text_view);
 
-  gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled_window), text_view);
-  gtk_window_set_child(GTK_WINDOW(window), scrolled_window);
+  scrolled_preview = gtk_scrolled_window_new();
+  preview_view = gtk_text_view_new();
+  gtk_text_view_set_editable(GTK_TEXT_VIEW(preview_view), FALSE);
+  gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled_preview), preview_view);
+
+  gtk_paned_set_start_child(GTK_PANED(paned), scrolled_editor);
+  gtk_paned_set_end_child(GTK_PANED(paned), scrolled_preview);
 
   app->window = window;
   app->text_view = text_view;
@@ -51,11 +111,20 @@ static void activate(GtkApplication *gapp, gpointer user_data) {
   app->current_file = NULL;
   app->modified = FALSE;
 
+  GtkTextBuffer *preview_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(preview_view));
+
+  GtkTextTagTable *tag_table = gtk_text_buffer_get_tag_table(preview_buffer);
+  GtkTextTag *heading1 = gtk_text_tag_new("heading1");
+  g_object_set(heading1,
+               "weight", PANGO_WEIGHT_BOLD,
+               "scale", 1.5, NULL);
+  gtk_text_tag_table_add(tag_table, heading1);
+
   g_signal_connect(app->buffer, "changed", G_CALLBACK(on_buffer_changed), app);
+  g_signal_connect(app->buffer, "changed", G_CALLBACK(on_editor_changed), preview_buffer);
 
   GtkCssProvider *provider = gtk_css_provider_new();
   GFile *css_file = g_file_new_for_path("../assets/cutwater.css");
-
   gtk_css_provider_load_from_file(provider, css_file);
   g_object_unref(css_file);
 
