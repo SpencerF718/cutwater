528b4973bd5f90f1018c8cbf6df16773b6a879e2
SpencerF718 <194403957+SpencerF718@users.noreply.github.com>
2025-10-15 20:53:36 -0400

    Add vim header outline

---
+++ include/vim.h
@@ -0,0 +1,21 @@
+#ifdef VIM_H
+#define VIM_H
+
+#include "cutwater.h"
+
+// Holds Vim mode
+typedef enum {
+  NORMAL_MODE,
+  INSERT_MODE,
+} VimMode;
+
+// Holds state information
+typedef struct {
+  CutwaterApp *app,
+  VimMode mode;
+} VimState;
+
+// Initializes Vim motion implementation and integrates with text view
+void vim_init(Cutwater *app);
+
+#endif
