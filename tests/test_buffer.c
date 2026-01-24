6d649c52bff25e6e835eeb9390ae4b44669ae9fd
SpencerF718 <194403957+SpencerF718@users.noreply.github.com>
2026-01-24 15:27:51 -0500

    Remove some magic numbers

--- tests/test_buffer.c
+++ tests/test_buffer.c
@@ -2,17 +2,27 @@
 #include <assert.h>
 #include <stdio.h>
 
+#define BUFFER_SUCCESS 0
+#define INITIAL_CAPACITY 10
+
+#define SMALL_CAPACITY 2
+#define GROWTH_FACTOR 2
+
+#define TEST_CHAR_1 'A'
+#define TEST_CHAR_2 'B'
+#define TEST_CHAR_3 'C'
+
 void test_buffer_init(void) {
     printf("TESTING: buffer_init\n");
 
     EditorBuffer eb;
 
-    int buffer_init_result = buffer_init(&eb, 10);
-    assert(buffer_init_result == 0);
+    int buffer_init_result = buffer_init(&eb, INITIAL_CAPACITY);
+    assert(buffer_init_result == BUFFER_SUCCESS);
 
-    assert(eb.capacity == 10);
+    assert(eb.capacity == INITIAL_CAPACITY);
     assert(eb.gap_start == 0);
-    assert(eb.gap_end == 10);
+    assert(eb.gap_end == INITIAL_CAPACITY);
     assert(eb.data != NULL);
 
     buffer_free(&eb);
@@ -23,14 +33,18 @@ void test_buffer_grow(void) {
     printf("TESTING: buffer_grow\n");
 
     EditorBuffer eb;
-    buffer_init(&eb, 2);
-    buffer_insert(&eb, 'A');
-    buffer_insert(&eb, 'B');
-    buffer_insert(&eb, 'C');
 
-    assert(eb.capacity == 4);
-    assert(eb.gap_start == 3);
-    assert(eb.gap_end == 4);
+    buffer_init(&eb, SMALL_CAPACITY);
+    buffer_insert(&eb, TEST_CHAR_1);
+    buffer_insert(&eb, TEST_CHAR_2);
+    buffer_insert(&eb, TEST_CHAR_3);
+
+    size_t expected_capacity = SMALL_CAPACITY * GROWTH_FACTOR;
+    size_t items_inserted = 3;
+
+    assert(eb.capacity == expected_capacity);
+    assert(eb.gap_start == items_inserted); 
+    assert(eb.gap_end == expected_capacity);
     assert(eb.data != NULL);
 
     buffer_free(&eb);
@@ -41,14 +55,16 @@ void test_buffer_insert(void) {
     printf("TESTING: buffer_insert\n");
 
     EditorBuffer eb;
-    buffer_init(&eb, 10);
+    buffer_init(&eb, INITIAL_CAPACITY);
 
-    int buffer_insert_result = buffer_insert(&eb, 'A');
-    assert(buffer_insert_result == 0);
+    int buffer_insert_result = buffer_insert(&eb, TEST_CHAR_1);
+    assert(buffer_insert_result == BUFFER_SUCCESS);
 
-    assert(eb.capacity == 10);
-    assert(eb.gap_start == 1);
-    assert(eb.gap_end == 10);
+    size_t expected_gap_start = 1;
+
+    assert(eb.capacity == INITIAL_CAPACITY);
+    assert(eb.gap_start == expected_gap_start);
+    assert(eb.gap_end == INITIAL_CAPACITY);
     assert(eb.data != NULL);
 
     buffer_free(&eb);
@@ -60,15 +76,15 @@ void test_buffer_delete(void) {
     printf("TESTING: buffer_delete\n");
 
     EditorBuffer eb;
-    buffer_init(&eb, 10);
-    buffer_insert(&eb, 'A');
+    buffer_init(&eb, INITIAL_CAPACITY);
+    buffer_insert(&eb, TEST_CHAR_1);
 
     int buffer_delete_result = buffer_delete(&eb);
-    assert(buffer_delete_result == 0);
+    assert(buffer_delete_result == BUFFER_SUCCESS);
 
-    assert(eb.capacity == 10);
+    assert(eb.capacity == INITIAL_CAPACITY);
     assert(eb.gap_start == 0);
-    assert(eb.gap_end == 10);
+    assert(eb.gap_end == INITIAL_CAPACITY);
     assert(eb.data != NULL);
 
     buffer_free(&eb);
@@ -79,18 +95,22 @@ void test_buffer_move_left(void) {
     printf("TESTING: buffer_move_left\n");
 
     EditorBuffer eb;
-    buffer_init(&eb, 10);
-    buffer_insert(&eb, 'A');
-    buffer_insert(&eb, 'B');
+    buffer_init(&eb, INITIAL_CAPACITY);
+    buffer_insert(&eb, TEST_CHAR_1);
+    buffer_insert(&eb, TEST_CHAR_2);
 
     int buffer_move_left_result = buffer_move_left(&eb);
-    assert(buffer_move_left_result == 0);
+    assert(buffer_move_left_result == BUFFER_SUCCESS);
 
-    assert(eb.capacity == 10);
-    assert(eb.gap_start == 1);
-    assert(eb.gap_end == 9);
-    assert(eb.data[0] == 'A');
-    assert(eb.data[9] == 'B');
+    size_t expected_gap_start = 1;
+    size_t expected_gap_end = INITIAL_CAPACITY - 1;
+
+    assert(eb.capacity == INITIAL_CAPACITY);
+    assert(eb.gap_start == expected_gap_start);
+    assert(eb.gap_end == expected_gap_end);
+
+    assert(eb.data[0] == TEST_CHAR_1);
+    assert(eb.data[expected_gap_end] == TEST_CHAR_2);
     assert(eb.data != NULL);
 
     buffer_free(&eb);
@@ -101,19 +121,21 @@ void test_buffer_move_right(void) {
     printf("TESTING: buffer_move_right\n");
 
     EditorBuffer eb;
-    buffer_init(&eb, 10);
-    buffer_insert(&eb, 'A');
-    buffer_insert(&eb, 'B');
+    buffer_init(&eb, INITIAL_CAPACITY);
+    buffer_insert(&eb, TEST_CHAR_1);
+    buffer_insert(&eb, TEST_CHAR_2);
     buffer_move_left(&eb);
 
     int buffer_move_right_result = buffer_move_right(&eb);
-    assert(buffer_move_right_result == 0);
+    assert(buffer_move_right_result == BUFFER_SUCCESS);
 
-    assert(eb.capacity == 10);
-    assert(eb.gap_start == 2);
-    assert(eb.gap_end == 10);
-    assert(eb.data[0] == 'A');
-    assert(eb.data[1] == 'B');
+    size_t expected_gap_start = 2;
+
+    assert(eb.capacity == INITIAL_CAPACITY);
+    assert(eb.gap_start == expected_gap_start);
+    assert(eb.gap_end == INITIAL_CAPACITY);
+    assert(eb.data[0] == TEST_CHAR_1);
+    assert(eb.data[1] == TEST_CHAR_2);
     assert(eb.data != NULL);
 
     buffer_free(&eb);
