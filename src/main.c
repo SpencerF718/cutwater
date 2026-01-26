08a5720b02db005f292190abdc7fa7cd39266b27
SpencerF718 <194403957+SpencerF718@users.noreply.github.com>
2026-01-26 18:40:59 -0500

    Add text rendering from buffer

--- src/main.c
+++ src/main.c
@@ -1,18 +1,40 @@
 #include <ncurses.h>
 #include <locale.h>
+#include <stdio.h>
+#include "tui.h"
+#include "buffer.h"
+
+#define INITIAL_CAPACITY 10
 
 int main (void) {
     setlocale(LC_ALL, "");
 
-    initscr();
+    EditorBuffer eb;
+    if (buffer_init(&eb, INITIAL_CAPACITY) != 0) {
+        fprintf(stderr, "Failed to initialize buffer\n");
+        return 1;
+    }
 
+    char *text = "Hello World\n";
+    for (int i = 0; text[i] != '\0'; i++) {
+        buffer_insert(&eb, text[i]);
+    }
+
+    initscr();
     raw();
     noecho();
     keypad(stdscr, TRUE);
 
-    printw("test test test 123 123 123");
+    while (1) {
+        clear();
+        render_buffer(&eb);
+        refresh();
+        int ch = getch();
 
-    getch();
+        if (ch == 'q') {
+            break;
+        }
+    }
 
     endwin();
     return 0;
