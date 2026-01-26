1ff443f95f3138cb8d3c76eafef000851072746a
SpencerF718 <194403957+SpencerF718@users.noreply.github.com>
2026-01-26 17:37:41 -0500

    ncursesw setup and working

--- src/main.c
+++ src/main.c
@@ -1,3 +1,19 @@
-int main () {
+#include <ncurses.h>
+#include <locale.h>
+
+int main (void) {
+    setlocale(LC_ALL, "");
+
+    initscr();
+
+    raw();
+    noecho();
+    keypad(stdscr, TRUE);
+
+    printw("test test test 123 123 123");
+
+    getch();
+
+    endwin();
     return 0;
 }
